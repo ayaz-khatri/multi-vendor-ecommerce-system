edf28289b522127398fbb62fdf16a5530ceeff64
ayaz-khatri <ayaz.khatri919@gmail.com>
2025-12-26 19:17:10 +0500

    Vendor and Products displayed on home page.

--- controllers/frontendController.js
+++ controllers/frontendController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js";
+import Product from "../models/Product.js";
 import errorMessage from "../utils/error-message.js";
 // import { validationResult } from "express-validator";
 // import path from 'path';
@@ -6,7 +7,10 @@ import errorMessage from "../utils/error-message.js";
 
 const index = async (req, res, next) => {
     try {
-        res.render("frontend/index", { title: "Ecommerce - Online Shopping Website" });
+        const vendors = await User.find({ role: "vendor", isDeleted: false }).limit(10);
+        const products = await Product.find({ isDeleted: false }).limit(8).populate('shopId', 'name').populate('categoryId', 'name').populate('vendorId', 'name');
+        // res.send(products);
+        res.render("frontend/index", { vendors, products, title: "Ecommerce - Online Shopping Website" });
     } catch (error) {
         next(errorMessage("Something went wrong", 500));
     }
